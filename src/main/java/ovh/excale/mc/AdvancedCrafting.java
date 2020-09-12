450df9a6c288e652612c5a42d1c78fb522ff6b46
Excale <dev_excale@hotmail.com>
2020-09-12 02:31:20 +0200

    Load examples in plugin data folder

--- src/main/java/ovh/excale/mc/AdvancedCrafting.java
+++ src/main/java/ovh/excale/mc/AdvancedCrafting.java
@@ -6,9 +6,7 @@ import ovh.excale.mc.advcraft.CraftingRecipe;
 import ovh.excale.mc.advcraft.exceptions.ArgumentParseException;
 import ovh.excale.mc.advcraft.exceptions.MissingArgumentException;
 
-import java.io.File;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
 import java.util.logging.Logger;
 
 public class AdvancedCrafting extends JavaPlugin {
@@ -16,6 +14,10 @@ public class AdvancedCrafting extends JavaPlugin {
 	private static Plugin plugin;
 	private static File craftDir;
 	private static boolean debug;
+
+	private static String[] EXAMPLES = new String[] {
+			"/examples/Shaped.yml", "/examples/Potion Shapeless.yml"
+	};
 
 	public static Plugin plugin() {
 		return plugin;
@@ -28,14 +30,38 @@ public class AdvancedCrafting extends JavaPlugin {
 		plugin = this;
 
 		File configFile = new File(getDataFolder(), "config.yml");
-		if(!configFile.exists())
+		if(!configFile.exists()) {
+			logger.info("Couldn't find config file, creating a new one");
 			saveDefaultConfig();
+		}
 
 		debug = getConfig().getBoolean("debug");
-		craftDir = new File(getDataFolder(), "crafts");
 
+		File exDir = new File(getDataFolder(), "examples");
+		if(!exDir.exists() && !exDir.mkdirs() || !exDir.isDirectory())
+			logger.warning("Couldn't get examples dir, aborting examples loading");
+		else {
+
+			for(String fileName : EXAMPLES) {
+				try(InputStream is = getClass().getResourceAsStream(fileName); OutputStream os = new FileOutputStream(new File(
+						getDataFolder(),
+						fileName.substring(1)))) {
+
+					int length;
+					byte[] buffer = new byte[1024];
+					while ((length = is.read(buffer)) > 0) {
+						os.write(buffer, 0, length);
+					}
+
+				} catch(Exception e) {
+					logger.severe(stringStackTrace(e));
+				}
+			}
+		}
+
+		craftDir = new File(getDataFolder(), "crafts");
 		if(!craftDir.exists() && !craftDir.mkdirs() || !craftDir.isDirectory()) {
-			getLogger().severe("Couldn't get crafts dir, aborting all actions.");
+			logger.severe("Couldn't get crafts dir, aborting all actions");
 			return;
 		}
 
@@ -55,14 +81,16 @@ public class AdvancedCrafting extends JavaPlugin {
 			} catch(ArgumentParseException | MissingArgumentException e) {
 				logger.warning(e.getMessage());
 
-				if(debug) {
-					StringWriter sw = new StringWriter();
-					PrintWriter pw = new PrintWriter(sw);
-					e.printStackTrace(pw);
-
-					logger.severe(sw.toString());
-				}
+				if(debug)
+					logger.severe(stringStackTrace(e));
 			}
+	}
+
+	private static String stringStackTrace(Exception e) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		e.printStackTrace(pw);
+		return sw.toString();
 	}
 
 }
